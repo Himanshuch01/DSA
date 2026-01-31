ef62b2c6949746e03c90dadf010a5b4991cb630e
Himanshu-0143 <143328985+Himanshu-0143@users.noreply.github.com>
2026-01-31 09:38:27 +0530

    Move Zeroes

---
+++ Class_practice/passing_arr.cpp
@@ -0,0 +1,45 @@
+// Create an insertion_at_end function first take user input for n(arraysize) create and array of that size and insert n elements taken as user input by using insertion_at_end function 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Arithmetic{
+  private:
+      int op1, op2;
+  public:
+      Arithmetic(int a, int b){
+        //this keyword is usefull when refering the data members of the class
+        this->op1=a;
+        this->op2 = b;
+
+      }
+      int add(){
+        return this->op1+this->op2;
+
+      }
+};
+
+
+//write a code which creates a class with makrs of 5 students using an array and a function to add marks for a particular students and display marks for a particular student . By default student maeks for all the 5 students should be 0.
+
+class student_record{
+  public:
+  student_Record(int def){
+    void add_marks(int score, int ind){
+      marks[i]
+    }
+  }
+}
+
+
+//function overloading
+
+class overloading{
+
+}
+int main (){
+  overloading a
+}
+// area of circle = pie*r*r
+// area of rectangle = l*b;
