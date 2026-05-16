d52803aec2437f0362f356a12bce19e5a69d2186
Himanshu-0143 <143328985+Himanshu-0143@users.noreply.github.com>
2026-05-16 08:35:41 +0530

    Practice

---
+++ Rotated_sorted_array_II.java
@@ -0,0 +1,36 @@
+public class Rotated_sorted_array_II {
+
+
+        public int findMin(int[] nums) {
+            // Initialize two pointers for binary search
+            int left = 0;
+            int right = nums.length - 1;
+
+            // Continue searching while the search space has more than one element
+            while (left < right) {
+                // Calculate the middle index using bit shift (equivalent to division by 2)
+                int mid = (left + right) >> 1;
+
+                // Case 1: Middle element is greater than rightmost element
+                // This means the minimum is in the right half (rotation point is on the right)
+                if (nums[mid] > nums[right]) {
+                    left = mid + 1;
+                }
+                // Case 2: Middle element is less than rightmost element
+                // This means the right half is sorted, so minimum is in left half (including mid)
+                else if (nums[mid] < nums[right]) {
+                    right = mid;
+                }
+                // Case 3: Middle element equals rightmost element
+                // Cannot determine which half contains minimum, safely shrink from right
+                else {
+                    right--;
+                }
+            }
+
+            // When left equals right, we've found the minimum element
+            return nums[left];
+        }
+
+}
+
